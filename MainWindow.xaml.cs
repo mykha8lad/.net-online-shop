f073c06700e5b7abd28aa12512eabdf0436f7073
Vladyslav Mykhailichenko <mykhailichenkovlad@gmail.com>
2023-10-24 14:08:11 +0300

    Project init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace online_store
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection? sqlConnection = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["StoreDB"].ConnectionString);
+            sqlConnection.Open();
+
+            if(sqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Подключение установлено");
+            }
+        }
+    }
+}
+
+
+
+// Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\c#-projects\wpfapp\online-store\online-store\OnlineStoreDB.mdf;Integrated Security=True
